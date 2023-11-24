4a867137876d5f145d64b00366d4a655b3056da6
Battsetseg Batbaatar <23LP3707@G304-CC0037.local>
2023-11-24 19:44:43 +0800

    dhakjs

---
+++ to-do.js
@@ -0,0 +1,22 @@
+const addBtnRef = document.getElementById("addBtn");
+const backdrop = document.getElementsByClassName("backdrop");
+console.log(addBtnRef);
+
+addBtnRef.addEventListener("click", (event) => {
+  const active = document.querySelectorAll(".active");
+  console.log("click", active);
+  active.forEach((element) => {
+    element.style.display = "flex";
+  });
+});
+
+const btnRef = document.getElementById("btn");
+function check() {
+  const titleInputRef = document.getElementById("title");
+  const disInputRef = document.getElementById("description");
+  if (titleInputRef.value === "" && disInputRef.value === "") {
+    btnRef.addEventListener("click", () => {
+      document.body.style = "warning";
+    });
+  }
+}
