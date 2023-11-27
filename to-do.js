2e4f22d1723d46241e8e5023db02207b6c318ee1
Battsetseg Batbaatar <23LP3707@G304-CC0037.local>
2023-11-27 20:12:59 +0800

    to-do

--- to-do.js
+++ to-do.js
@@ -2,6 +2,7 @@ const addBtnRef = document.getElementById("addBtn");
 const backdrop = document.getElementsByClassName("backdrop");
 console.log(addBtnRef);
 
+// + add button der darahad tsonh vvsch task hiih heseg
 addBtnRef.addEventListener("click", (event) => {
   const active = document.querySelectorAll(".active");
   console.log("click", active);
@@ -9,14 +10,80 @@ addBtnRef.addEventListener("click", (event) => {
     element.style.display = "flex";
   });
 });
+// taskaa hiih hooson array
+const state = {
+  tasks: [],
+};
+// shine tsonhon deer utgaa awhiin tuld id duudsan
+const titleInputRef = document.getElementById("title");
+const desInputRef = document.getElementById("description");
+const prioInputRef = document.getElementById("priority");
+const staIputRef = document.getElementById("status");
+const deleteBtn = document.getElementById("delete");
+const i = 0;
+console.log(titleInputRef.value);
+function render() {
+  i++;
+  //  vvsgesen arrayruu utga hiisn
+  tasks.push({
+    Title: titleInputRef,
+    description: desInputRef,
+    state: staIputRef,
+    priority: prioInputRef,
+    count: i,
+  });
+  state.tasks.forEach((task) => {
+    //  shine elment vvsgesen
+    const div = document.createElement("div");
+    const done = document.createElement("div");
+    const defailt = document.createElement("div");
+    const edit = document.createElement("div");
+    const titRef = document.createElement("h1");
+    const desRef = document.createElement("p");
+    const statusRef = document.createElement("div");
+    const prioRef = document.createElement("div");
 
+    // shine vvsgesen elementendee tsonhon deer bichsen utgaa oruulsn
+    titRef.textContent = titleInputRef.value;
+    desRef.textContent = desInputRef.value;
+    statusRef.textContent = staIputRef.value;
+    prioRef.textContent = prioInputRef.value;
+
+    div.appendChild(done);
+    div.appendChild(defailt);
+    div.appendChild(edit);
+
+    const card = document.getElementsByClassName("cards");
+    card.appendChild(task);
+    if (statusRef.value === "todo") {
+      const todo = document.getElementById("card");
+      todo.appendChild(card);
+    }
+    if (statusRef.value === "inprogress") {
+      const inProgress = document.getElementById(inProgress);
+      inProgress.appendChild(card);
+    }
+    if (statusRef.value === "stuck") {
+      const stuck = document.getElementById(stuck);
+      stuck.appendChild(card);
+    }
+    if (statusRef.value === "done") {
+      const done = document.getElementById(done);
+      done.appendChild(card);
+    }
+    deleteBtn.onclick = function () {};
+  });
+}
 const btnRef = document.getElementById("btn");
+btnRef.onclick = function () {
+  task.completed = !task.completed;
+  render();
+};
 function newTask() {
-  const titleInputRef = document.getElementById("title");
-  const disInputRef = document.getElementById("description");
-  if (titleInputRef.value === "" && disInputRef.value === "") {
+  if (titleInputRef.value === "" && desInputRef.value === "") {
     btnRef.addEventListener("click", () => {
       document.body.style = "warning";
     });
   }
+  render();
 }
