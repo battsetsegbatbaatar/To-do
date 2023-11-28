764d7e033c3e7e9556cd90533b232f0bfbe5717b
Battsetseg Batbaatar <23LP3707@G304-CC0037.local>
2023-11-28 21:43:27 +0800

    card ordog bolsoo

--- to-do.js
+++ to-do.js
@@ -1,89 +1,123 @@
 const addBtnRef = document.getElementById("addBtn");
-const backdrop = document.getElementsByClassName("backdrop");
-console.log(addBtnRef);
 
 // + add button der darahad tsonh vvsch task hiih heseg
+const active = document.querySelectorAll(".active");
 addBtnRef.addEventListener("click", (event) => {
-  const active = document.querySelectorAll(".active");
   console.log("click", active);
   active.forEach((element) => {
     element.style.display = "flex";
   });
+  console.log(state.tasks);
 });
 // taskaa hiih hooson array
 const state = {
   tasks: [],
 };
+const addTaskBtn = document.getElementById("btn");
+addTaskBtn.addEventListener("click", () => {
+  const active = document.querySelector(".active");
+  if (active) {
+    active.style.display = "none";
+  }
+  addTask();
+});
+
 // shine tsonhon deer utgaa awhiin tuld id duudsan
 const titleInputRef = document.getElementById("title");
 const desInputRef = document.getElementById("description");
 const prioInputRef = document.getElementById("priority");
 const staIputRef = document.getElementById("status");
 const deleteBtn = document.getElementById("delete");
-const i = 0;
-console.log(titleInputRef.value);
+
 function render() {
-  i++;
+  console.log(state.task);
   //  vvsgesen arrayruu utga hiisn
-  tasks.push({
-    Title: titleInputRef,
-    description: desInputRef,
-    state: staIputRef,
-    priority: prioInputRef,
-    count: i,
-  });
   state.tasks.forEach((task) => {
     //  shine elment vvsgesen
-    const div = document.createElement("div");
+    const card = document.createElement("div");
     const done = document.createElement("div");
     const defailt = document.createElement("div");
     const edit = document.createElement("div");
-    const titRef = document.createElement("h1");
+    const titRef = document.createElement("h4");
     const desRef = document.createElement("p");
     const statusRef = document.createElement("div");
     const prioRef = document.createElement("div");
 
     // shine vvsgesen elementendee tsonhon deer bichsen utgaa oruulsn
-    titRef.textContent = titleInputRef.value;
-    desRef.textContent = desInputRef.value;
-    statusRef.textContent = staIputRef.value;
-    prioRef.textContent = prioInputRef.value;
+    titRef.textContent = task.Title;
+    desRef.textContent = task.description;
+    statusRef.textContent = task.state;
+    prioRef.textContent = task.priority;
+    console.log(titRef);
+    console.log(task);
+    // icon nuudaa duudsan
+    const doneIcon = document.createElement("i");
+    doneIcon.classList.add("far", "circle-check");
+    const deleteIcon = document.createElement("i");
+    deleteIcon.classList.add("fa-solid", "fa-xmark");
+    const editIcon = document.createElement("i");
+    editIcon.classList.add("fa-regular", "fa-pen-to-square");
 
-    div.appendChild(done);
-    div.appendChild(defailt);
-    div.appendChild(edit);
+    // duudsan zvilsee appenad child hiisen
+    done.appendChild(doneIcon);
+    done.classList.add("done");
+    defailt.appendChild(titRef);
+    defailt.appendChild(desRef);
+    defailt.appendChild(prioRef);
+    prioRef.classList.add("priority");
+    defailt.classList.add("defailt");
+    edit.appendChild(deleteIcon);
+    edit.appendChild(editIcon);
+    edit.classList.add("edit");
+    card.appendChild(done);
+    card.appendChild(defailt);
+    card.appendChild(edit);
+    card.classList.add("card");
+    console.log(card);
 
-    const card = document.getElementsByClassName("cards");
-    card.appendChild(task);
-    if (statusRef.value === "todo") {
-      const todo = document.getElementById("card");
-      todo.appendChild(card);
+    console.log(task.state);
+    if ((task.completed = !task.completed)) {
+      // 4 baganaruu oruulah heseg
+      if (task.state === "todo") {
+        const todo = document.getElementById("todo");
+        todo.appendChild(card);
+        console.log(todo);
+      }
+      if (task.state === "inprogress") {
+        const inProgress = document.getElementById("inProgress");
+        inProgress.innerHTML = "";
+        inProgress.appendChild(card);
+      }
+      if (task.state === "stuck") {
+        const stuck = document.getElementById("stuck");
+        stuck.innerHTML = "";
+        stuck.appendChild(card);
+      }
+      if (task.state === "done") {
+        const done = document.getElementById("done");
+        done.innerHTML = "";
+        done.appendChild(card);
+      }
     }
-    if (statusRef.value === "inprogress") {
-      const inProgress = document.getElementById(inProgress);
-      inProgress.appendChild(card);
-    }
-    if (statusRef.value === "stuck") {
-      const stuck = document.getElementById(stuck);
-      stuck.appendChild(card);
-    }
-    if (statusRef.value === "done") {
-      const done = document.getElementById(done);
-      done.appendChild(card);
-    }
-    deleteBtn.onclick = function () {};
   });
 }
-const btnRef = document.getElementById("btn");
-btnRef.onclick = function () {
-  task.completed = !task.completed;
-  render();
-};
-function newTask() {
-  if (titleInputRef.value === "" && desInputRef.value === "") {
-    btnRef.addEventListener("click", () => {
-      document.body.style = "warning";
-    });
+
+const addTask = () => {
+  state.tasks.push({
+    Title: titleInputRef.value.trim(),
+    description: desInputRef.value.trim(),
+    state: staIputRef.value,
+    priority: prioInputRef.value,
+  });
+  const warning = document.getElementById("warning");
+  if (titleInputRef.value === "" || desInputRef.value === "") {
+    warning.style.display = "flex";
+  } else {
+    warning.style.display = "none";
   }
   render();
-}
+  titleInputRef.value = "";
+  desInputRef.value = "";
+  staIputRef.value = "";
+  prioInputRef.value = "";
+};
